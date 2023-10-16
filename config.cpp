b129f9cbe12250bd3c49db28c3423e8c5fe0ba73
GeorgeAlfonso <280772butakov@gmail.com>
2023-10-16 21:29:59 +1000

    fix

--- config.cpp
+++ config.cpp
@@ -32,67 +32,21 @@ class CfgWeapons
 		baseWeapon = "DIV7_OPTRE_M392_DMR";
 		displayName = "[DIV7] M392 DMR";
 		canShootInWater = 1;
-		magazineWell[] = {};
-		magazines[] = {"DIV7__Mag_762x51mm_15Rnd_APFSDS_Tracer", "DIV7__Mag_762x51mm_15Rnd_APFSDS", "OPTRE_15Rnd_762x51_Mag", "OPTRE_15Rnd_762x51_Mag_Tracer", "OPTRE_15Rnd_762x51_Mag_Tracer_Yellow"};
-		class WeaponSlotsInfo : WeaponSlotsInfo
-		{
-			class MuzzleSlot : MuzzleSlot
-			{
-				compatibleitems[] = {"muzzle_snds_h_mg_blk_f", "muzzle_snds_l", "optre_ma5suppressor", "optre_m7_silencer", "optre_m6_silencer", "ace_muzzle_mzls_b", "muzzle_snds_b", "OPTRE_MA37KSuppressor", "muzzle_snds_65_TI_blk_F", "OPTRE_srs99d_suppressor"};
-			};
-			class CowsSlot : CowsSlot
-			{
-				compatibleitems[] = {"optic_Yorris", "optic_DMS", "optic_DMS_ghex_F", "optic_ERCO_blk_F", "optic_ERCO_khk_F", "optic_ERCO_snd_F", "optic_MRCO", "optic_Arco", "optic_Arco_ghex_F", "optic_Arco_blk_F", "optic_aco", "optic_ACO_grn", "optic_aco_smg", "optic_ACO_grn_smg", "optic_hamr", "optic_Hamr_khk_F", "optic_Holosight", "optic_Holosight_smg", "optic_Holosight_blk_F", "optic_Holosight_khk_F", "optic_Holosight_smg_blk_F", "optic_Holosight_smg_khk_F", "OPTRE_M392_Scope", "OPTRE_BR55HB_Scope", "OPTRE_M7_Sight", "OPTRE_M393_Scope", "OPTRE_M393_ACOG", "OPTRE_M393_EOTECH", "OPTRE_M73_SmartLink", "OPTRE_MA5_SmartLink", "OPTRE_MA5_SmartLink_v2", "OPTRE_MA5C_SmartLink_v2", "OPTRE_MA5_SmartLink_legacy", "OPTRE_MA5C_SmartLink_legacy", "OPTRE_M12_Optic", "OPTRE_M12_Optic_Red", "OPTRE_M12_Optic_Green", "optic_ico_01_black_f", "OPTRE_BR45_Scope", "optic_NVS", "optic_LRPS", "optic_LRPS_ghex_F", "optic_LRPS_tna_F", "optic_ams", "optic_AMS_snd", "optic_AMS_khk", "optic_KHS_blk", "optic_KHS_tan", "optic_KHS_hex", "optic_KHS_old", "optic_SOS", "optic_SOS_khk_F", "OPTRE_M392_Scope_v2", "OPTRE_M393_ACOG_v2", "OPTRE_M393_EOTECH_v2", "OPTRE_BMR_Scope_v2"};
-			};
-			class PointerSlot : PointerSlot
-			{
-				compatibleitems[] = {"OPTRE_M7_Flashlight", "optre_dmr_light", "OPTRE_M7_Laser", "OPTRE_M7_Vis_Red_Laser", "OPTRE_BMR_Laser", "OPTRE_BMR_MEQ_Flashlight", "OPTRE_BMR_Vis_Red_Laser", "optre_m12_laser", "OPTRE_M12_Vis_Red_Laser", "OPTRE_M12_Flashlight", "optre_m45_flashlight", "optre_m45_flashlight_red", "OPTRE_M6C_Laser", "OPTRE_M6C_Vis_Red_Laser", "OPTRE_M6C_Flashlight", "OPTRE_M6G_Laser", "OPTRE_M6G_Vis_Red_Laser", "OPTRE_M6G_Flashlight", "acc_pointer_ir", "acc_flashlight", "ace_acc_pointer_green"};
-			};
-		};
+		magazines[] += {"DIV7__Mag_762x51mm_15Rnd_APFSDS_Tracer", "DIV7__Mag_762x51mm_15Rnd_APFSDS"};
 	};
 
 	class DIV7_M45A : OPTRE_M45A
 	{
 		baseWeapon = "DIV7_OPTRE_M45A";
 		displayName = "[DIV7] M45A";
-		magazineWell[] = {};
-		magazines[] = {"OPTRE_12Rnd_8Gauge_Pellets", "OPTRE_6Rnd_8Gauge_Pellets", "OPTRE_12Rnd_8Gauge_Slugs", "OPTRE_6Rnd_8Gauge_Slugs", "DIV7_12Rnd_8Gauge_Pellets_M45"};
-		class WeaponSlotsInfo : WeaponSlotsInfo{
-				class MuzzleSlot : MuzzleSlot{
-						compatibleitems[] = {"muzzle_snds_h_mg_blk_f", "muzzle_snds_l", "optre_ma5suppressor", "optre_m7_silencer", "optre_m6_silencer", "ace_muzzle_mzls_b", "muzzle_snds_b", "OPTRE_MA37KSuppressor", "muzzle_snds_65_TI_blk_F", "OPTRE_srs99d_suppressor", "OPTRE_srs99d_suppressor"};
-			};
-			class CowsSlot: CowsSlot
-			{
-				compatibleitems[] = {"optic_Yorris","optic_DMS","optic_DMS_ghex_F","optic_ERCO_blk_F","optic_ERCO_khk_F","optic_ERCO_snd_F","optic_MRCO","optic_Arco","optic_Arco_ghex_F","optic_Arco_blk_F","optic_aco","optic_ACO_grn","optic_aco_smg","optic_ACO_grn_smg","optic_hamr","optic_Hamr_khk_F","optic_Holosight","optic_Holosight_smg","optic_Holosight_blk_F","optic_Holosight_khk_F","optic_Holosight_smg_blk_F","optic_Holosight_smg_khk_F","OPTRE_M392_Scope","OPTRE_BR55HB_Scope","OPTRE_M7_Sight","OPTRE_M393_Scope","OPTRE_M393_ACOG","OPTRE_M393_EOTECH","OPTRE_M73_SmartLink","OPTRE_MA5_SmartLink","OPTRE_MA5_SmartLink_v2","OPTRE_MA5C_SmartLink_v2","OPTRE_MA5_SmartLink_legacy","OPTRE_MA5C_SmartLink_legacy","OPTRE_M12_Optic","OPTRE_M12_Optic_Red","OPTRE_M12_Optic_Green","optic_ico_01_black_f","OPTRE_BR45_Scope"};
-			};
-			class PointerSlot: PointerSlot
-			{
-				compatibleitems[] = {"OPTRE_M7_Flashlight","OPTRE_M7_Laser","OPTRE_M7_Vis_Red_Laser","OPTRE_BMR_Laser","OPTRE_BMR_MEQ_Flashlight","OPTRE_BMR_Vis_Red_Laser","optre_m12_laser","OPTRE_M12_Vis_Red_Laser","OPTRE_M12_Flashlight","optre_m45_flashlight","optre_m45_flashlight_red","OPTRE_M6C_Laser","OPTRE_M6C_Vis_Red_Laser","OPTRE_M6C_Flashlight","OPTRE_M6G_Laser","OPTRE_M6G_Vis_Red_Laser","OPTRE_M6G_Flashlight","acc_pointer_ir","acc_flashlight","ace_acc_pointer_green","acc_pointer_vis_red"};
-			};
-		};
+		magazines[] += {"DIV7_12Rnd_8Gauge_Pellets_M45"};
 	};
 
 	class DIV7_M90A : OPTRE_M90A
 	{
 		baseWeapon = "DIV7_OPTRE_M90A";
 		displayName = "[DIV7] M90";
-		magazineWell[] = {};
-		magazines[] = {"OPTRE_12Rnd_8Gauge_Pellets", "OPTRE_6Rnd_8Gauge_Pellets", "OPTRE_12Rnd_8Gauge_Slugs", "OPTRE_6Rnd_8Gauge_Slugs", "DIV7_12Rnd_8Gauge_Pellets_M90"};
-		class WeaponSlotsInfo : WeaponSlotsInfo
-		{
-			class MuzzleSlot : MuzzleSlot
-			{
-				compatibleitems[] = {"muzzle_snds_h_mg_blk_f", "muzzle_snds_l", "optre_ma5suppressor", "optre_m7_silencer", "optre_m6_silencer", "ace_muzzle_mzls_b", "muzzle_snds_b", "OPTRE_MA37KSuppressor", "muzzle_snds_65_TI_blk_F", "OPTRE_srs99d_suppressor", "OPTRE_srs99d_suppressor"};
-			};
-			class CowsSlot : CowsSlot
-			{
-				compatibleitems[] = {};
-			};
-			class PointerSlot : PointerSlot
-			{
-				compatibleitems[] = {"OPTRE_M7_Flashlight", "OPTRE_M7_Laser", "OPTRE_M7_Vis_Red_Laser", "OPTRE_BMR_Laser", "OPTRE_BMR_MEQ_Flashlight", "OPTRE_BMR_Vis_Red_Laser", "optre_m12_laser", "OPTRE_M12_Vis_Red_Laser", "OPTRE_M12_Flashlight", "optre_m45_flashlight", "optre_m45_flashlight_red", "OPTRE_M6C_Laser", "OPTRE_M6C_Vis_Red_Laser", "OPTRE_M6C_Flashlight", "OPTRE_M6G_Laser", "OPTRE_M6G_Vis_Red_Laser", "OPTRE_M6G_Flashlight", "acc_pointer_ir", "acc_flashlight", "ace_acc_pointer_green", "acc_pointer_vis_red"};
-			};
-		};
+		magazines[] += {"DIV7_12Rnd_8Gauge_Pellets_M90"};
 	};
 };
 
