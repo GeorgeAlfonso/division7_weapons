d38311bc6663426dd3b391f943d27c0b06ce0fbb
GeorgeAlfonso <280772butakov@gmail.com>
2023-10-16 22:32:00 +1000

    Добавлены пистолеты, M247 и БК к ним.

--- config.cpp
+++ config.cpp
@@ -11,7 +11,7 @@ class CfgPatches
 		magazines[] = {};
 		ammo[] = {};
 		requiredVersion = 0.1;
-		requiredAddons[] = {"division7_core", "OPTRE_Weapons"};
+		requiredAddons[] = {"division7_core", "OPTRE_Weapons", "TCF_WEAPONRY", "TCF_MUNITIONS"};
 	};
 };
 
@@ -26,7 +26,58 @@ class CfgWeapons
 	class OPTRE_M45A;
 	class OPTRE_M90A;
 	class OPTRE_M392_DMR;
-	
+
+	class OPTRE_M247H_Etilka;
+
+	class OPTRE_M6B;
+	class OPTRE_M6G;
+	class OPTRE_M6C;
+	class TCF_M6D;
+
+	// Pistols
+
+	class DIV7_OPTRE_M6B : OPTRE_M6B
+	{
+		baseWeapon = "DIV7_OPTRE_M6B";
+		displayName = "[DIV7] M6B";
+		magazines[] += {"DIV7_12Rnd_127x40_Mag_JHP"};
+	};
+
+	class DIV7_OPTRE_M6C : OPTRE_M6C
+	{
+		baseWeapon = "DIV7_OPTRE_M6C";
+		displayName = "[DIV7] M6C";
+		magazines[] += {"DIV7_16Rnd_127x40_Mag"};
+	};
+
+	class DIV7_M6D : TCF_M6D
+	{
+		baseWeapon = "DIV7_M6D";
+		displayName = "[DIV7] M6D";
+		magazines[] += {"DIV7_12Rnd_127x40_Mag_SAPHE", "DIV7_12Rnd_127x40_Mag_JHP"};
+	};
+
+	class DIV7_OPTRE_M6G : OPTRE_M6G
+	{
+		baseWeapon = "DIV7_OPTRE_M6G";
+		displayName = "[DIV7] M6G";
+		magazines[] += {"DIV7_12Rnd_127x40_Mag_JHP"};
+	};
+
+	// Etilka
+
+	class DIV7_M247H_Etilka : OPTRE_M247H_Etilka
+	{
+		baseWeapon = "DIV7_M247H_Etilka";
+		displayName = "[DIV7] M247H HMG ""Etilka""";
+		dlc = "DIV7";
+		scope = 2;
+		scopeArsenal = 2;
+		magazines[] += { "DIV7_200Rnd_127x99_M247H_Etilka_Ball" };
+	};
+
+	//DMR
+
 	class DIV7_OPTRE_M392_DMR : OPTRE_M392_DMR
 	{
 		baseWeapon = "DIV7_OPTRE_M392_DMR";
@@ -34,6 +85,8 @@ class CfgWeapons
 		canShootInWater = 1;
 		magazines[] += {"DIV7__Mag_762x51mm_15Rnd_APFSDS_Tracer", "DIV7__Mag_762x51mm_15Rnd_APFSDS"};
 	};
+
+	//Shotguns
 
 	class DIV7_M45A : OPTRE_M45A
 	{
@@ -48,14 +101,21 @@ class CfgWeapons
 		displayName = "[DIV7] M90";
 		magazines[] += {"DIV7_12Rnd_8Gauge_Pellets_M90"};
 	};
+
+
+
 };
 
 class CfgAmmo
 	{
 		class OPTRE_8Gauge_Pellets;
+		class OPTRE_B_127x40_Ball;
 		class B_762x51_Ball;
+		class TCF_B_127x40_SAPHE;
+		class TCF_B_127x40_JHP;
 
-		class DIV7_762x51_Ball : B_762x51_Ball {
+		class DIV7_762x51_Ball : B_762x51_Ball
+		{
 			caliber = 2.1;
 			hit = 24;
 			typicalSpeed = 750;
@@ -82,11 +142,30 @@ class CfgAmmo
 			model = "\A3\Weapons_f\Data\bullettracer\tracer_red";
 		};
 
+		class DIV7_127x40_SAPHE : TCF_B_127x40_SAPHE
+		{
+			hit = 32;
+			caliber = 2.5;
+		};
+
+		class DIV7_127x40_JHP : TCF_B_127x40_JHP
+		{
+			hit = 32;
+			caliber = 1;
+		};
+
+		class DIV7_127x40_Ball : OPTRE_B_127x40_Ball
+		{
+			hit = 20;
+			caliber = 2;
+		};
 	};
 
 class CfgMagazines
 {
+
 	class 20Rnd_762x51_Mag;
+	class OPTRE_200Rnd_127x99_M247H_Etilka_Ball;
 
 	class DIV7__Mag_762x51mm_15Rnd_APFSDS : 20Rnd_762x51_Mag
 	{
@@ -135,6 +214,55 @@ class CfgMagazines
 		displayname = "[DIV7] 12Rnd 8 Gauge Pellets M45";
 		ammo = "DIV7_8Gause_Pellets_M45";
 		descriptionshort = "12 Rounds of 8 Gauge Pellets M45";
+	};
+
+	class DIV7_16Rnd_127x40_Mag : 20Rnd_762x51_Mag
+	{
+		dlc = "DIV7";
+		hiddenSelections[] = {"camo1"};
+		hiddenSelectionsTextures[] = {"\optre_weapons\pistol\data\M6G_CO.paa"};
+		displayname = "16Rnd 12.7x40mm Magazine";
+		displaynameshort = "12.7x40mm";
+		ammo = "DIV7_127x40_Ball";
+		count = 16;
+		initspeed = 600;
+		picture = "\OPTRE_weapons\pistol\icons\magazine.paa";
+		descriptionshort = "8 Round Magazine<br/>12.7x40mm<br/>Semi-Armor Piercing High Explosive";
+		mass = 9;
+		tracersEvery = 0;
+		lastRoundsTracer = 2;
+		model = "\OPTRE_Weapons\Pistol\m6_magazine.p3d";
+		modelSpecial = "\OPTRE_Weapons\Pistol\m6_magazine.p3d";
+		modelSpecialIsProxy = 1;
+	};
+
+	class DIV7_12Rnd_127x40_Mag_SAPHE : DIV7_16Rnd_127x40_Mag
+	{
+		dlc = "DIV7";
+		displayname = "12Rnd 12.7x40mm SAPHE Magazine";
+		displaynameshort = "12.7x40mm SAPHE";
+		ammo = "DIV7_127x40_SAPHE";
+		count = 12;
+		initspeed = 620;
+		descriptionshort = "12 Round Magazine<br/>12.7x40mm<br/>Semi-Armor Piercing High Explosive";
+		mass = 9;
+	};
+
+	class DIV7_12Rnd_127x40_Mag_JHP : DIV7_16Rnd_127x40_Mag
+	{
+		dlc = "DIV7";
+		displayname = "12Rnd 12.7x40mm JHP Magazine";
+		displaynameshort = "12.7x40mm JHP";
+		ammo = "DIV7_127x40_JHP";
+		count = 12;
+		initspeed = 580;
+		descriptionshort = "12 Round Magazine<br/>12.7x40mm<br/>Jacketed Hollow-Point";
+	};
+
+	class DIV7_200Rnd_127x99_M247H_Etilka_Ball : OPTRE_200Rnd_127x99_M247H_Etilka_Ball 
+	{
+		dlc = "DIV7";
+		mass = 75;
 	};
 
 };
